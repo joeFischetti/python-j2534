e2f7c2838df7c56a9cd81acc04569f39037d4282
Joe Fischetti <jkfischetti@gmail.com>
2021-02-28 00:42:37 -0500

    uppated j2534 and connections with ioctl functions for stmin

--- test_connection.py
+++ test_connection.py
@@ -1,4 +1,5 @@
 from connections import J2534Connection
+from connections import FakeConnection
 import logging
 import time
 
@@ -10,20 +11,24 @@ logger.addHandler(f_handler)
 
 
 conn = J2534Connection(windll='C:/Program Files (x86)/OpenECU/OpenPort 2.0/drivers/openport 2.0/op20pt32.dll', rxid=0x7E8, txid=0x7E0)
+#conn = FakeConnection()
 conn.open()
 
-data = [b'\x10\x03',b'\x22\xf1\x89',b'\x01\x06', b'\x22\xf1\x91',b'\x01\x0c',b'\x01\x04',b'\x01\x05']
+conn.txid = 0x700
+conn.resetCable()
+
+data = [b'\x22\xf1\x90\xf1\x89\xf1\x91\xf8\x06\xf1\xa3']
 
 
-for param in data:
-    logger.debug("Requesting: " + str(param.hex()))
-    conn.send(param)
-    response = None
-    #while response is None:
-    response = conn.wait_frame()
-    if response:
-        print("Response: " + str(response.hex()))
-
+for i in range(0, 1):
+    for param in data:
+        logger.debug("Requesting: " + str(param.hex()))
+        conn.send(param)
+        response = None
+        #while response is None:
+        response = conn.wait_frame()
+        if response:
+            print("Response: " + str(response.hex()))
 
 conn.close()
 
